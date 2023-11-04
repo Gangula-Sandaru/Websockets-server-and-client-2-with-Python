1e2df7f741109dcbe2eb1509b7d9cc86b78ff4fe
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-11-04 10:03:51 +0530

    finished the show time py

---
+++ show_time.py
@@ -0,0 +1,20 @@
+import asyncio
+import datetime
+import random
+import websockets
+
+
+async def show_time(websocket):
+    while True:
+        message = datetime.datetime.utcnow().isoformat() + "z"
+        await websocket.send(message)
+        await asyncio.sleep(random.random() * 2 + 1)
+
+
+async def main():
+    async with websockets.serve(show_time, "localhost", 5678):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
