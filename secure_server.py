1f12cdb3f86ba9ded1de0d8cdcf43ebfed5e24d3
Gangula Sandaru Dinusantha <119031640+Gangula-Sandaru@users.noreply.github.com>
2023-11-04 09:32:36 +0530

    finished the secure server

---
+++ secure_server.py
@@ -0,0 +1,28 @@
+import asyncio
+import pathlib
+import ssl
+import websockets
+
+
+async def hello(websocket):
+    name = await websocket.recv()
+    print(f"<<<{name}")
+
+    greeting = f"hello {name}"
+
+    await websocket.send(greeting)
+    print(f">>>{greeting}")
+
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+ssl_context.load_cert_chain(localhost_pem)
+
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765, ssl=ssl_context):
+        await asyncio.Future()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
